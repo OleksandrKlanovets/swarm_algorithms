9bc0a4640b1862702324e44e664d8335f2247f0e
Alexander Klanovets <sanchofeed@gmail.com>
2019-09-25 21:44:07 +0300

    Modified FA, refactored the code and added some comments

--- firefly_algorithm/firefly.py
+++ firefly_algorithm/firefly.py
@@ -5,28 +5,51 @@ import random as rand
 # Class, which represents a single firefly in a swarm.
 class Firefly:
     # D - dimension of a search space;
-    # p_range - range of coordinates' values (from -range to range).
-    def __init__(self, D, p_range):
+    # p_range - range of coordinates' values (from -range to range);
+    # fit_func - object function.
+    def __init__(self, D, p_range, fit_func):
         self.D = D
         self.p_range = p_range
         self.coordinates = np.random.rand(D) * p_range
         self.coordinates *= np.power(np.full(D, -1), np.random.randint(2, size = D))
+        self.score = fit_func(self.coordinates)
 
     # Gets attractiveness for another firefly.
     def __beta(self, firefly, beta_zero, gamma):
         # Euclidian distance between two fireflies.
         distance = np.linalg.norm(self.coordinates - firefly.coordinates)
         # We can rewrite the attractiveness function as follows:
-        #   beta_zero * np.exp(-gamma * distance * distance)
-        return beta_zero / (1 + gamma * distance * distance)
+        #   beta_zero * np.exp(-gamma * distance ** 2)
+        # But to improve the perfomance, we define it as:
+        return beta_zero / (1 + gamma * distance ** 2)
 
     # Move a firefly towards another one.
-    def move_towards_firefly(self, firefly, beta_zero, alfa, gamma, lambd, gbest_pos):
+    def move_towards_firefly(self, firefly, beta_zero, alfa, gamma, lambd, gbest_pos, fit_func):
+        # Get the attractiveness of the firefly we want to fly to.
         current_beta = self.__beta(firefly, beta_zero, gamma)
+
+        # Get vector difference of fireflies' coordinates.
         diff = self.coordinates - firefly.coordinates
+
+        # Get vector of random values in range (-1; 1).
         urand_n = np.multiply(
             np.power(np.full(self.D, -1), np.random.randint(2, size = self.D)), 
             np.random.rand(self.D)
         )
+
+        # Update coordinates according to the flight formula.
         self.coordinates = firefly.coordinates + current_beta * diff + alfa * urand_n
+
+        # Modifications:
+        # Get vector of random values in range (-1; 1).
+        urand_n = np.multiply(
+            np.power(np.full(self.D, -1), np.random.randint(2, size = self.D)), 
+            np.random.rand(self.D)
+        )
+
+        # Add an extra term to the flight formula, which uses global best solution
+        # to improve the efficiency.
         self.coordinates += lambd * np.multiply(urand_n, self.coordinates - gbest_pos)
+
+        # Calculate the fit score (brightness) of the firefly based on it's new position.
+        self.score = fit_func(self.coordinates)
--- firefly_algorithm/firefly_swarm.py
+++ firefly_algorithm/firefly_swarm.py
@@ -9,10 +9,10 @@ class FireflySwarm:
     # beta_0 - attractiveness of fireflies at 0 distance;
     # alfa_0, alfa_inf - initial and final value of alfa parameter respectively;
     # gamma - light absorption coefficient;
-    # fitness_func - target function to optimize;
+    # fit_func - target function to optimize;
     # iter_num - maximum number of iterations;
     # p_range - range of fireflies coordinates values (from -range to range);
-    def __init__(self, D, N, beta_0, alfa_0, alfa_inf, gamma, lambd, fitness_func, iter_num, p_range):
+    def __init__(self, D, N, beta_0, alfa_0, alfa_inf, gamma, lambd, fit_func, iter_num, p_range):
         self.D = D
         self.N = N
         self.beta_0 = beta_0
@@ -20,55 +20,58 @@ class FireflySwarm:
         self.alfa_inf = alfa_inf
         self.gamma = gamma
         self.lambd = lambd
-        self.fitness_func = fitness_func
+        self.fit_func = fit_func
         self.iter_num = iter_num
         self.p_range = p_range
-        self.fireflies = [Firefly(D, p_range) for _ in range(N)]
+        self.fireflies = [Firefly(D, p_range, fit_func) for _ in range(N)]
 
         # Setting random firefly as group's initial best position (we'll see if we need that).
         rnd_firefly = self.fireflies[np.random.randint(N)]
         self.group_best_position = np.copy(rnd_firefly.coordinates)
-        self.group_best_score = fitness_func(rnd_firefly.coordinates)
+        self.group_best_score = fit_func(rnd_firefly.coordinates)
 
     # Updates best group score (is called at each iteration).
     def __update_group_best(self):
         for f in self.fireflies:
-            curr_score = self.fitness_func(f.coordinates)
+            curr_score = self.fit_func(f.coordinates)
             if curr_score < self.group_best_score:
                 self.group_best_score = curr_score
                 self.group_best_position = np.copy(f.coordinates)
 
-    # Gets fitness function values vector for all the fireflies.
-    def __get_fitness(self):
-        result_vec = []
-        for f in self.fireflies:
-            result_vec.append(self.fitness_func(f.coordinates))
-        return result_vec
+    # Update position of all the fireflies in the swarm.
+    def __move_all(self, alfa):
+        for i in range(self.N):
+            for j in range(self.N):
+                if self.fireflies[j].score < self.fireflies[i].score:
+                    # Move less attractive to more attractive.
+                    self.fireflies[i].move_towards_firefly(
+                        self.fireflies[j],
+                        self.beta_0,
+                        alfa,
+                        self.gamma,
+                        self.lambd,
+                        self.group_best_position,
+                        self.fit_func
+                    )
+
+    # Returns a key for fireflies' ranking.
+    def __score_key(self, val):
+        return val.score
 
     def optimize(self):
         t = 0
         alfa = self.alfa_0
-        # fitness = self.__get_fitness()
         while(t < self.iter_num):
             # Loop through all fireflies pairs.
-            for i in range(self.N):
-                for j in range(0, i):
-                    f_i = self.fireflies[i].coordinates
-                    f_j = self.fireflies[j].coordinates
-                    # Compare their attractiveness.
-                    # if fitness[j] < fitness[i]:
-                    if self.fitness_func(f_j) < self.fitness_func(f_i):
-                        # Move less attractive to more attractive.
-                        self.fireflies[i].move_towards_firefly(
-                            self.fireflies[j],
-                            self.beta_0,
-                            alfa,
-                            self.gamma,
-                            self.lambd,
-                            self.group_best_position
-                        )
-            alfa = self.alfa_inf + (self.alfa_0 - self.alfa_inf) * np.exp(-t)
-            # fitness = self.__get_fitness()
-            t += 1
+            self.__move_all(alfa)
+
+            # Rank the fireflies.
+            self.fireflies.sort(key=self.__score_key)
+
+            # Find current best.
             self.__update_group_best()
+            
+            # Update alfa.
+            alfa = self.alfa_inf + (self.alfa_0 - self.alfa_inf) * np.exp(-t)
+            t += 1
         return self.group_best_position
